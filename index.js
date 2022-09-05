55500a8dacb8b4885fd2cdd7cac01218d6d75666
Karan Kalani <karankalani75@gmail.com>
2022-09-05 16:48:59 +0530

    ma estartGame button and some styling

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-let firstNumber = 6
-let secondNumber = 9
+let firstNumber =3
+let secondNumber = 2
 let total = firstNumber + secondNumber
 let hasBlackjack = false
 let isAlive = true
 let message = ''
 
-if (total < 21) {
-    message = 'Do you want to draw a new card ?'
-} else if (total === 21) {
-    message = "You've got blackjack"
-    hasBlackjack = true
-} else {
-    message = "You're out of the game!"
-    isAlive = false
+function startGame() {
+    if (total < 21) {
+        message = 'Do you want to draw a new card ?'
+    } else if (total === 21) {
+        message = "You've got blackjack"
+        hasBlackjack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
 }
-
 console.log(message)
 console.log(hasBlackjack)
