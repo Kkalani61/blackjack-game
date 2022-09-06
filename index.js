5a8278fad71583d82853bde287213dd373a63e54
Karan Kalani <karankalani75@gmail.com>
2022-09-06 21:41:06 +0530

    Built blackjack game

--- index.js
+++ index.js
@@ -1,20 +1,62 @@
-let firstNumber =11
-let secondNumber = 10
-let total = firstNumber + secondNumber
+let cards = []
+let sum = 0
 let hasBlackjack = false
-let isAlive = true
+let isAlive = false
 let message = ''
 
+let player = {
+    name : 'Karan',
+    chips : '145'
+}
+
+document.getElementById('player').textContent = player.name + ' : $' + player.chips
+
+function newCard() {
+    if (isAlive === true && hasBlackjack === false) {
+        message = 'Drawing new card from the deck'
+        document.getElementById('message').textContent = message
+        let newNumber = getRandomCard()
+        sum += newNumber
+        cards.push(newNumber)
+        renderGame()
+    }
+}
+
 function startGame() {
-    if (total < 21) {
+    isAlive = true
+    let firstNumber = getRandomCard()
+    let secondNumber = getRandomCard()
+    cards = [firstNumber, secondNumber]
+    sum = firstNumber + secondNumber
+    renderGame()
+}
+
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+    if (randomNumber >= 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
+
+function renderGame() {
+    if (sum < 21) {
         message = 'Do you want to draw a new card ?'
-    } else if (total === 21) {
+    } else if (sum === 21) {
         message = "You've got blackjack"
         hasBlackjack = true
     } else {
         message = "You're out of the game!"
         isAlive = false
     }
-    document.getElementById('message-el').textContent = message
+    document.getElementById('message').textContent = message
+    document.querySelector('#sum').textContent = 'Sum : ' +sum
+    document.getElementById('card').textContent = 'Cards : '
+    for (i=0; i<cards.length; i++) {
+        document.getElementById('card').textContent += cards[i] + ' '
+    }
+    
 }
-
